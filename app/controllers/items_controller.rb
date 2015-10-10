76649baa8541444ce020e1ce84a16da661d6a6b7
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2015-10-10 17:58:41 +0900

    Show only the header of table when giving empty string as keyword

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
   def index
-    @items = RWS::Ichiba::Item.
-      search(keyword: params[:keyword], imageFlag: 1).first(10)
+    @items = if params[:keyword].present?
+       RWS::Ichiba::Item.
+        search(keyword: params[:keyword], imageFlag: 1).first(10)
+    else
+      []
+    end
   end
 end
