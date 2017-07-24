7845fb56e459d4bfea5646708bf7be521d89b936
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2017-07-24 18:08:37 +0900

    Define spec ItemsController#index

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe ItemsController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should assigns items as an empty array" do
+      get :index
+
+      expect(assigns[:items]).to be_empty
+    end
+    
+    context 'When given keyword' do
+      let(:keyword) { 'ruby' }
+      let(:items) { 10.times.map { double(:item) } }
+
+      before do
+        expect(RakutenWebService::Ichiba::Item).to receive(:search).with(keyword: keyword, imageFlag: 1).and_return(items)
+
+        get :index, keyword: keyword
+      end
+
+      it "should return http success" do
+        expect(response).to have_http_status(:success)
+      end
+      it "should assign returned 10 objects as items" do
+        expect(assigns[:items]).to be_eql(items)
+      end
+    end
+  end
+end
