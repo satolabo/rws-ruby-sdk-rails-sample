54d568561342b19624615886321f606e82b38ebe
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2017-07-30 18:18:28 +0900

    Apply bootstrap framework to the existing views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   def index
-    @items = if params[:keyword].present?
+    @keyword = params[:keyword]
+
+    @items = if @keyword.present?
       RWS::Ichiba::Item.
         search(keyword: params[:keyword], imageFlag: 1).first(10)
     else
